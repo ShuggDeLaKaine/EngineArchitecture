08a3070e08af7bb1a60a3d308603055b2d334309
Simon Coupland <simonc@dmu.ac.uk>
2020-06-19 10:00:11 +0100

    Change example test.

--- engineTests/src/smokeTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "smokeTest.h"
-#include <Windows.h>
-
-TEST(SmokeTest, OpenAndClose) {
-	int exitCode = callAndGetResult("start ../bin/Debug-windows/Sandbox/Sandbox.exe");
-	//int exitCode = callAndGetResult("cd");
-	EXPECT_EQ(exitCode, 0);
-}
