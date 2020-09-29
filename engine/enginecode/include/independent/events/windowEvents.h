6649e784b55e350d513d495d375436e6ae791c67
Tom Hodgson <tomhodgson55@gmail.com>
2020-09-29 14:06:41 +0100

    windowEvents.h
    Added to WindowMoveEvent an main function, get functions for the x & y floats and the x & y floats for movement themselves.
    Doxygen commented througout.
    Compiled and ran, so no errors, ready to go.
    
    NEED - main functions for the WindowFocus and WindowLostFocuse classes.
    Should look into what is required for these further.

--- engine/enginecode/include/independent/events/windowEvents.h
+++ engine/enginecode/include/independent/events/windowEvents.h
@@ -24,14 +24,12 @@ namespace Engine
 	class WindowResizeEvent : public Event
 	{
 	public:
-		WindowResizeEvent(int32_t width, int32_t height) : m_width(width), m_height(height) { }	//!< using initaliser list as will set width/height straight to m_width/m_height rather than to 0 first.
 		static EventType getStaticType() { return EventType::WindowResize; }					//!< returns static type; reason for static type is that is gives me something to compare with when implementing a handler.
 		virtual inline EventType getEventType() const override { return EventType::WindowResize; }	//!< get the event type.
 		virtual int32_t getCategoryFlag() const override { return EventCategoryWindow; }			//!< get the category flag for the event.
-
+		WindowResizeEvent(int32_t width, int32_t height) : m_width(width), m_height(height) { }	//!< using initaliser list as will set width/height straight to m_width/m_height rather than to 0 first.
 		inline int32_t getWidth() const { return m_width; }		//!< get the width of window.
 		inline int32_t getHeight() const { return m_height; }	//!< get the height of window.
-
 	private:
 		int32_t m_width;		//!< window width after resize.
 		int32_t m_height;		//!< window height after resize.
@@ -43,11 +41,10 @@ namespace Engine
 	class WindowFocusEvent : public Event
 	{
 	public:
-
 		static EventType getStaticType() { return EventType::WindowFocus; }		//!< returns static type; reason for static type is that is gives me something to compare with when implementing a handler.
 		virtual inline EventType getEventType() const override { return EventType::WindowFocus; }	//!< get the event type.
 		virtual int32_t getCategoryFlag() const override { return EventCategoryWindow; }			//!< get the category flag for the event.
-	private:
+		//NEED - initialised function for the Window Focus.
 
 	};
 
@@ -57,11 +54,10 @@ namespace Engine
 	class WindowLostFocusEvent : public Event
 	{
 	public:
-
 		static EventType getStaticType() { return EventType::WindowLostFocus; }		//!< returns static type; reason for static type is that is gives me something to compare with when implementing a handler.
 		virtual inline EventType getEventType() const override { return EventType::WindowLostFocus; }	//!< get the event type.
 		virtual int32_t getCategoryFlag() const override { return EventCategoryWindow; }				//!< get the category flag for the event.
-	private:
+		//NEED - initialised function for the Window Lost Focus.
 
 	};
 
@@ -74,9 +70,18 @@ namespace Engine
 		static EventType getStaticType() { return EventType::WindowMove; }		//!< returns static type; reason for static type is that is gives me something to compare with when implementing a handler.
 		virtual inline EventType getEventType() const override { return EventType::WindowMove; }	//!< get the event type.
 		virtual int32_t getCategoryFlag() const override { return EventCategoryWindow; }			//!< get the category flag for the event.
-	private:
-		//position moved to...
 
+		//NEED - initialised function for the Window Move.
+		WindowMoveEvent(const float x, const float y) : windowMoveX(x), windowMoveY(y) {}		//!< initaliser, listed the x/y straight to windowMoveX/Y.
+
+		//NEED - get functions for float x & y for window movement.
+		inline float getWindowXMove() { return windowMoveX; }		//!< get the float for window movement on the X axis.
+		inline float getWindowYMove() { return windowMoveY; }		//!< get the float for window movement on the Y axis.
+
+	private:
+		//NEED - floats for new window position on the x and the y
+		float windowMoveX;		//!< float for X position of window movement.
+		float windowMoveY;		//!< float for Y position of window movement.
 	};
 }
 
