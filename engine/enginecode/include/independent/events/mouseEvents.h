4a1d8b797ba93ab91c6fba4a5917eceb4f5092e5
Tom Hodgson <tomhodgson55@gmail.com>
2020-09-29 16:24:44 +0100

    mouseEvents.h Cleaned up the file. Added class MouseEvent, to take the getCategoryFlag() which all the other classes share.

--- engine/enginecode/include/independent/events/mouseEvents.h
+++ engine/enginecode/include/independent/events/mouseEvents.h
@@ -7,16 +7,22 @@
 namespace Engine
 {
 	/**
+	\class MouseEvent - class to take a number of funcs and vars that will be common in all mouse event classes.
+	*/
+	class MouseEvent : public Event
+	{
+		virtual int32_t getCategoryFlag() const override { return EventCategoryMouse | EventCategoryInput; }	//!< get the category flag for the event.
+	};
+
+	/**
 	\class MouseButtonPressEvent - class for mouse button press event.
 	*/
-	class MouseButtonPressEvent : public Event
+	class MouseButtonPressEvent : public MouseEvent
 	{
 	public:
-
-		static EventType getStaticType() { return EventType::MouseButtonPress; }	//!< get the static event type, can use to compare.
-		virtual inline EventType getEventType() const override { return EventType::MouseButtonPress; }	//!< get the event type.
-		virtual int32_t getCategoryFlag() const override { return EventCategory::EventCategoryMouse; }	//!< get the category flag for the event.
 		MouseButtonPressEvent(const int b) : m_Button(b) {}			//!< using initaliser list to set button straight to m_Button.
+		static EventType getStaticType() { return EventType::MouseButtonPress; }	//!< get the static event type, can use to compare.
+		virtual inline EventType getEventType() const override { return getStaticType(); }	//!< get the event type.
 		inline int getButtonPressed() const { return m_Button; }	//!< get the mouse button pressed.
 	private:
 		int m_Button;		//!< int for mouse button pressed.
@@ -25,13 +31,12 @@ namespace Engine
 	/**
 	\class MouseButtonReleaseEvent - class for mouse button release event.
 	*/
-	class MouseButtonReleaseEvent : public Event
+	class MouseButtonReleaseEvent : public MouseEvent
 	{
 	public:
-		static EventType getStaticType() { return EventType::MouseButtonRelease; }	//!< get the static event type, can use to compare.
-		virtual inline EventType getEventType() const override { return EventType::MouseButtonRelease; }	//!< get the event type.
-		virtual int32_t getCategoryFlag() const override { return EventCategory::EventCategoryMouse; }		//!< get the category flag for the event.
 		MouseButtonReleaseEvent(const int b) : m_Button(b) {}		//!< using initaliser list to set button straight to m_Button.
+		static EventType getStaticType() { return EventType::MouseButtonRelease; }	//!< get the static event type, can use to compare.
+		virtual inline EventType getEventType() const override { return getStaticType(); }	//!< get the event type.
 		inline int getButtonReleased() const { return m_Button; }	//!< get the mouse button released.
 	private:
 		int m_Button;		//!< int for mouse button released.
@@ -40,13 +45,12 @@ namespace Engine
 	/**
 	\class MouseMouseEvent - class for mouse movement event.
 	*/
-	class MouseMovementEvent : public Event
+	class MouseMovementEvent : public MouseEvent
 	{
 	public:
-		static EventType getStaticType() { return EventType::MouseMovement; }	//!< get the static event type, can use to compare.
-		virtual inline EventType getEventType() const override { return EventType::MouseMovement; }		//!< get the event type.
-		virtual int32_t getCategoryFlag() const override { return EventCategory::EventCategoryMouse; }	//!< get the category flag for the event.
 		MouseMovementEvent(const float x, const float y) : m_mouseMoveX(x), m_mouseMoveY(y) {}		//!< using initaliser list to set x/y straight to m_mouseMoveX/Y.
+		static EventType getStaticType() { return EventType::MouseMovement; }	//!< get the static event type, can use to compare.
+		virtual inline EventType getEventType() const override { return getStaticType(); }		//!< get the event type.
 		inline float getXMovement() const { return m_mouseMoveX; }		//!< get the mouse movement coords on the X axis.
 		inline float getYMovement() const { return m_mouseMoveY; }		//!< get the mouse movement coords on the Y axis.
 	private:
@@ -57,13 +61,12 @@ namespace Engine
 	/**
 	\class MouseScrollEvent - class for mouse scroll event.
 	*/
-	class MouseScrollEvent : public Event
+	class MouseScrollEvent : public MouseEvent
 	{
 	public:
-		static EventType getStaticType() { return EventType::MouseScroll; }		//!< get the static event type, can use to compare.
-		virtual inline EventType getEventType() const override { return EventType::MouseScroll; }		//!< get the event type.
-		virtual int32_t getCategoryFlag() const override { return EventCategory::EventCategoryMouse; }	//!< get the category flag for the event.
 		MouseScrollEvent(const float x, const float y) : m_scrollX(x), m_scrollY(y) {}	//!< using initaliser list to set x/y straight to m_scrollX/Y.
+		static EventType getStaticType() { return EventType::MouseScroll; }		//!< get the static event type, can use to compare.
+		virtual inline EventType getEventType() const override { return getStaticType(); }		//!< get the event type.
 		inline float getXMouseScroll() const { return m_scrollX; }		//!< get the scroll X float.
 		inline float getYMouseScroll() const { return m_scrollY; }		//!< get the scroll Y float.
 	private:
